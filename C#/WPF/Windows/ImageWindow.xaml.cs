8636de4bb7719cfeb43041ad7f058b962559e978
Color_yr <402067010@qq.com>
2022-08-11 16:32:54 +0800

    up

---
+++ C#/WPF/Windows/ImageWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace ColoryrBuild.Windows;
+
+/// <summary>
+/// ImageWindow.xaml 的交互逻辑
+/// </summary>
+public partial class ImageWindow : Window
+{
+    public ImageWindow(byte[] data)
+    {
+        InitializeComponent();
+        using MemoryStream stream = new(data);
+        var bitmapImage = new BitmapImage();
+        bitmapImage.BeginInit();
+        bitmapImage.StreamSource = stream;
+        bitmapImage.EndInit();
+        Img.Source = bitmapImage;
+        Width = bitmapImage.Width;
+        Height = bitmapImage.Height;
+        Show();
+        Activate();
+    }
+}
---
+++ C#/WPF/Windows/InfoWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+
+namespace ColoryrBuild.Windows
+{
+    /// <summary>
+    /// InfoWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class InfoWindow : Window
+    {
+        public InfoWindow(string title, string text = null)
+        {
+            InitializeComponent();
+            Title = title;
+            Text.Text = text;
+            ShowDialog();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
